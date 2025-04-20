c7fdab557f6b297b5dee0c1d954b3f37c5669044
RugvedTamhan <92918609+RugvedTamhan@users.noreply.github.com>
2025-04-20 22:03:30 +0530

    Update Load_Data.py

--- Drone flight/Load_Data.py
+++ Drone flight/Load_Data.py
@@ -4,10 +4,8 @@ def load_path(file_path="Flight_paths.json", mission_index=0):
     with open(file_path, "r") as f:
         data = json.load(f)
     
-    # Load the selected primary mission
     primary = data["primary_missions"][mission_index]
     
-    # Load all other drones
     others = data["other_drones"]
     
     return primary, others
